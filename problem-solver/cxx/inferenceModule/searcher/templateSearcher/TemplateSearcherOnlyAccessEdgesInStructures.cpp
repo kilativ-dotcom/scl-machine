2adf837399c25d7e9af4a133c14dfe2bb091ab46
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-01-22 09:28:00 +0300

    [searcher] Calculate hashset on every invocation

--- problem-solver/cxx/inferenceModule/searcher/templateSearcher/TemplateSearcherOnlyAccessEdgesInStructures.cpp
+++ problem-solver/cxx/inferenceModule/searcher/templateSearcher/TemplateSearcherOnlyAccessEdgesInStructures.cpp
@@ -12,7 +12,6 @@ TemplateSearcherOnlyAccessEdgesInStructures::TemplateSearcherOnlyAccessEdgesInSt
     ScAddrVector const & otherInputStructures)
   : TemplateSearcherInStructures(context, otherInputStructures)
 {
-  this->contentOfAllInputStructures = std::make_unique<ScAddrHashSet>();
 }
 
 TemplateSearcherOnlyAccessEdgesInStructures::TemplateSearcherOnlyAccessEdgesInStructures(ScMemoryContext * context)
@@ -29,6 +28,15 @@ void TemplateSearcherOnlyAccessEdgesInStructures::searchTemplate(
   ScTemplate searchTemplate;
   if (context->HelperBuildTemplate(searchTemplate, templateAddr, templateParams))
   {
+    this->contentOfAllInputStructures->clear();
+    SC_LOG_DEBUG("start input structures processing");
+    for (auto const & inputStructure : inputStructures)
+    {
+      ScAddrVector const & edges =
+          utils::IteratorUtils::getAllWithType(context, inputStructure, ScType::EdgeAccess);
+      contentOfAllInputStructures->insert(edges.cbegin(), edges.cend());
+    }
+    SC_LOG_DEBUG("input structures processed, found " << contentOfAllInputStructures->size() << " edges");
     if (context->HelperCheckEdge(
             InferenceKeynodes::concept_template_with_links, templateAddr, ScType::EdgeAccessConstPosPerm))
     {
@@ -36,17 +44,6 @@ void TemplateSearcherOnlyAccessEdgesInStructures::searchTemplate(
     }
     else
     {
-      if (this->contentOfAllInputStructures->empty())
-      {
-        SC_LOG_DEBUG("start input processing");
-        for (auto const & inputStructure : inputStructures)
-        {
-          ScAddrVector const & edges =
-              utils::IteratorUtils::getAllWithType(context, inputStructure, ScType::EdgeAccess);
-          contentOfAllInputStructures->insert(edges.cbegin(), edges.cend());
-        }
-        SC_LOG_DEBUG("input processed, found " << contentOfAllInputStructures->size() << " edges");
-      }
       context->HelperSmartSearchTemplate(
           searchTemplate,
           [templateParams, &result, &variables, this](
@@ -71,7 +68,8 @@ void TemplateSearcherOnlyAccessEdgesInStructures::searchTemplate(
           },
           [this](ScAddr const & item) -> bool {
             // Filter result item by not being Access Edge or belonging to any of the input structures
-            return !context->GetElementType(item).BitAnd(ScType::EdgeAccess) || contentOfAllInputStructures->count(item);
+            return !context->GetElementType(item).BitAnd(ScType::EdgeAccess) ||
+                   contentOfAllInputStructures->count(item);
           });
     }
   }
@@ -79,5 +77,55 @@ void TemplateSearcherOnlyAccessEdgesInStructures::searchTemplate(
   {
     throw std::runtime_error("Template is not built.");
   }
+}
+
+void TemplateSearcherOnlyAccessEdgesInStructures::searchTemplateWithContent(
+    ScTemplate const & searchTemplate,
+    ScAddr const & templateAddr,
+    ScTemplateParams const & templateParams,
+    Replacements & result)
+{
+  ScAddrHashSet variables;
+  getVariables(templateAddr, variables);
+  std::map<std::string, std::string> linksContentMap = getTemplateLinksContent(templateAddr);
+
+  context->HelperSearchTemplate(
+      searchTemplate,
+      [templateParams, &result, &variables](ScTemplateSearchResultItem const & item) -> ScTemplateSearchRequest {
+        // Add search result item to the answer container
+        for (ScAddr const & variable : variables)
+        {
+          ScAddr argument;
+          if (item.Has(variable))
+          {
+            result[variable].push_back(item[variable]);
+          }
+          if (templateParams.Get(variable, argument))
+          {
+            result[variable].push_back(argument);
+          }
+        }
+        return ScTemplateSearchRequest::STOP;
+      },
+      [&linksContentMap, this](ScTemplateSearchResultItem const & item) -> bool {
+        // Filter result item by the same content and belonging to any of the input structures
+        if (!isContentIdentical(item, linksContentMap))
+          return false;
+        for (size_t i = 0; i < item.Size(); i++)
+        {
+          if (context->GetElementType(item[i]).BitAnd(ScType::EdgeAccess) &&
+              !contentOfAllInputStructures->count(item[i]))
+            return false;
+        }
+        return true;
+      });
+}
+
+map<std::string, std::string> TemplateSearcherOnlyAccessEdgesInStructures::getTemplateLinksContent(
+    ScAddr const & templateAddr)
+{
+  //TODO(kilativ-dotcom): need to decide what to return here. Input structures contain only access edges so there are
+  // no links in them and map should be empty?
+  return {};
 }
 }  // namespace inference
